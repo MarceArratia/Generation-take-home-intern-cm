a79f1c34ab4f7703bf67b3794fd832c9812c2b10
Marce Arratia <marratiaf@hotmail.com>
2019-04-05 11:53:41 -0300

    agregando fetch

--- src/App.js
+++ src/App.js
@@ -3,7 +3,27 @@ import './App.css';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 
+
+
+const style = {
+  width: '100%',
+  height: '80%'
+}
 class App extends Component {
+  componentDidMount(){
+    let url='./store_directory.json';
+    fetch(url, {
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+
+    })
+    .then(res => res.json())
+    .then(text => console.log(text))
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -13,13 +33,19 @@ class App extends Component {
          
       <Map google={this.props.google}
        zoom={13}
+       style={style}
           initialCenter={{
             lat:19.419444,lng:-99.145556
           }}
           onClick={this.onMapClicked}>
  
- <Marker position={{lat:19.4978,lng:-99.1269}} onClick={this.onMarkerClick}
-         name={'Mexico'} />
+ <Marker position={{lat:19.434322,lng:-99.099506}} onClick={this.onMarkerClick}
+         name={'Los burritos de Mike'}
+       />
+<Marker position={{lat:19.431994,lng:-99.131153}} onClick={this.onMarkerClick}
+         name={'Palacio Nacional'} />
+<Marker position={{lat:40.640771,lng:-74.016133}} onClick={this.onMarkerClick}
+         name={'Museo'} />     
 
  <InfoWindow onClose={this.onInfoWindowClose}>
   
